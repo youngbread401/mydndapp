716d41389b36d411bd0d6402be90a7e1a367c483
youngbread401 <forevergames1410@gmail.com>
2025-01-08 03:26:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,26 +6,31 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files (HTML, CSS, JS)
-app.use(express.static(__dirname));
+// Serve the static files from the "public" folder
+app.use(express.static('public'));
 
-// Handle socket connections
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    // Handle cell toggle from any user
-    socket.on('toggleCell', (data) => {
-        console.log('Cell toggle:', data);
-        io.emit('updateGrid', data.id, data.highlighted);  // Broadcast the updated highlight state
-    });
-
-    // Handle disconnection
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+// Route to serve the index.html page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-// Start the server
+// Listen for connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  // Listen for clicks from the client
+  socket.on('click', (data) => {
+    // Broadcast the click event to all other connected clients
+    socket.broadcast.emit('click', data);
+  });
+
+  // Handle user disconnect
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Start the server on port 3000
 server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+  console.log('Server is running on http://localhost:3000');
 });
